d3f61f2a17963175dcbe725cb50b725dd68ddae5
Enoch Pang <enokpang@gmail.com>
2018-03-13 20:05:36 -0400

    Add vim config

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-ln -s ~/.config/emacs.d ~/.emacs.d
+## Emacs
+# ln -s ~/.config/emacs.d ~/.emacs.d
+
+## Vim
+ln -s ~/.config/vim ~/.vim
+mkdir -p vim/tmp
+
+## Neovim
 mkdir -p nvim/autoload
 curl -o nvim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
