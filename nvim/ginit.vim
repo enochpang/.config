b7f1955aca5924037b2a5801088776738bf2e06e
Enoch Pang <enokpang@gmail.com>
2020-04-10 10:08:43 -0400

    Update ginit.vim

--- nvim/ginit.vim
+++ nvim/ginit.vim
@@ -1,1 +1,9 @@
-set mouse=a
+call plug#begin(stdpath('data') . '/plugged')
+Plug 'arcticicestudio/nord-vim'
+call plug#end()
+
+colorscheme nord
+
+if exists('g:fvim_loaded')
+    FVimFontLcdRender v:false
+endif
--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,7 @@
+## Emacs
+#New-Item -ItemType SymbolicLink -Path $Env:USERPROFILE/.emacs.d -Target emacs.d/
+
+## Neovim
 New-Item -ItemType SymbolicLink -Path $Env:LOCALAPPDATA/nvim -Target nvim/
-New-Item -ItemType SymbolicLink -Path $Env:USERPROFILE/.emacs.d -Target emacs.d/
 New-Item -ItemType Directory -Force -Path nvim/autoload
 Invoke-WebRequest "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" -OutFile "./nvim/autoload/plug.vim"
